815cfd615e4e22a03345d2c3548556e5589f8e73
Prayag Sridhar <53558950+PRAYAG2000n@users.noreply.github.com>
2025-04-15 10:00:00 -0400

    Update register_file.sv

--- RTL/register_file.sv
+++ RTL/register_file.sv
@@ -1,11 +1,11 @@
 module register_file (
-    input wire        clk,
-    input wire        reset,
+    input wire clk,
+    input wire reset,
     input wire [4:0]  read_reg1,
     input wire [4:0]  read_reg2,
     input wire [4:0]  write_reg,
     input wire [31:0] write_data,
-    input wire        reg_write,
+    input wire reg_write,
     
     output reg [31:0] read_data1,
     output reg [31:0] read_data2
